ad29647ae966b43908965afe0554b7fa2f716699
Jay Hamilton <jay@Jays-MBP.home>
2015-04-15 14:20:57 -0600

    Added a working delete button to the recipe_detail page

--- backend/apps/recipes/urls.py
+++ backend/apps/recipes/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
-from views import RecipeList
-urlpatterns = [
+from views import RecipeList, RecipeDetail
+
+urlpatterns = patterns('',
     url('^recipes/$', RecipeList.as_view(), name='recipe-list'),
-]
+    url('^recipes/(?P<pk>[0-9]+)/$', RecipeDetail.as_view(), name='recipe-detail'),
+)
--- frontend/app/app.js
+++ frontend/app/app.js
@@ -4,6 +4,7 @@
 angular.module('myApp', [
     'ngRoute',
     'myApp.recipes',
+    'myApp.recipeDetail',
     'myApp.view2',
     'myApp.version',
     'restangular'
